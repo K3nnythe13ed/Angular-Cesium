2e74896c642f62713994818d42d6ce90e4e9f9cd
Stefan Weirauch <Stefan Weirauch>
2017-01-10 11:20:44 +0100

    locationlist

--- src/app/cesium/cesium.component.ts
+++ src/app/cesium/cesium.component.ts
@@ -37,6 +37,7 @@ export class CesiumComponent implements OnInit {
     var viewer = this.cesiumViewer;
     let marker = this.newMarkerOnMap;
     let pinbuilder = this.pinBuilder;
+    let addToList = this.addToList;
     this._client.search({
       index: 'logstash-*',
       type: 'warehouse',
@@ -54,6 +55,7 @@ export class CesiumComponent implements OnInit {
 
         response.hits.hits.forEach((hit) => {
           marker(hit, viewer, pinbuilder);
+          addToList(hit, "locationlist", true, viewer)
         })
         console.log('All is well');
 
@@ -99,6 +101,83 @@ export class CesiumComponent implements OnInit {
 
 
 
+  }
+
+  addToList(hit, element, del, viewer) {
+
+    var locationlist = document.getElementById(element);
+    var li = document.createElement("LI");
+    li.style.width = "350px"
+
+
+
+    var a = document.createElement('a');
+
+    var linkText = document.createTextNode("" + hit._source.properties.AccountName);
+    a.appendChild(linkText);
+    a.title = "Location: " + hit._source.properties.AccountName;
+    a.href = "#";
+    a.onclick = function () {
+      viewer.flyTo(viewer.entities.getById(hit._source.properties.LocID),
+        {
+          orientation: {
+            heading: Cesium.Math.toRadians(175.0),
+            pitch: Cesium.Math.toRadians(-35.0),
+            roll: 0.0
+          }
+        }
+      )
+      return false;
+    };
+    li.appendChild(a);
+
+
+    var att = document.createAttribute("id");
+    att.value = "li " + hit._source.properties.LocID;
+    li.setAttributeNode(att);
+    if (del) {
+      var btn = document.createElement("BUTTON");        // Create a <button> element
+      var t = document.createTextNode("Delete");       // Create a text node
+      btn.appendChild(t);
+
+      btn.onclick = function () {
+        deleteLocation(hit._source.properties.LocID)
+      }
+
+
+      li.appendChild(btn);
+
+    }
+    locationlist.appendChild(li)
+
+
+    function deleteLocation(id) {
+      this.client.delete({
+        index: 'logstash-constant',
+        type: 'warehouse',
+        id: id
+      }, function (error, response) {
+        this.client.indices.refresh({
+          index: 'logstash-constant'
+        }, function (err, results) {
+
+          //removePinMarker(id)
+          removeFromList(id)
+        }
+
+        )
+      });
+    }
+
+    function removePinMarker(id) {
+
+      viewer.entities.remove(viewer.entities.getById(id))
+    }
+    function removeFromList(id) {
+      var li = document.getElementById("li " + id);
+      li.parentNode.removeChild(li);
+
+    }
   }
   ngOnInit() {
     this.pinBuilder = new Cesium.PinBuilder();
